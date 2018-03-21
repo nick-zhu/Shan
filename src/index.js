3254981f5edb3d90a49e41ea174735d63f1db2a3
Nick <hanzhang.zhu@hds.com>
2018-03-21 15:31:35 -0700

    recursively mount nested virtual elements

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
+import { create } from "domain";
+
 // Creaet a vElement
 
-function createVElement(tag, config) {
+function createVElement(tag, config, children=null) {
   const { className } = config;
 
   return {
     tag: tag,
+    props: {
+      children: children
+    },
     className: className,
     dom: null
   }
 }
 
 function mountVElement(vElement, parentDomNode) {
-  const { tag, className } = vElement;
+  const { tag, className, props } = vElement;
 
   // creat a native DOM node
   const domNode = document.createElement(tag);
@@ -23,11 +28,32 @@ function mountVElement(vElement, parentDomNode) {
     domNode.className = className;
   }
 
+  // if has children, recursively mount elements
+  if(props.children) {
+    props.children.forEach( ele => {
+      mountVElement(ele, domNode);
+    })
+  }
+
   parentDomNode.appendChild(domNode);
 }
 
 const root = document.querySelector('.root')
 
-const app = createVElement('div', {className: 'my-app'})
+const myApp = createVElement('div', { className: 'my-class' }, [
+  createVElement('h1', { className:'my-header' }),
+  createVElement('div', { className:'my-container' }, [
+    createVElement('div', { className:'my-sub-container' }, [
+      createVElement('div', { className:'my-sub-sub-container' }, [
+        createVElement('div', { className:'my-sub-sub-sub-container' }, [
+          createVElement('div', { className:'my-sub-sub-sub-sub-container' }, [
+            createVElement('div', { className:'my-sub-sub-sub-sub-sub-container' }, []),
+            createVElement('h1', { className:'my-sub-sub-sub-sub-sub-header' }, [])
+          ])
+        ])
+      ])
+    ]),
+  ])
+]);
 
-mountVElement(app, root);
+mountVElement(myApp, root);
